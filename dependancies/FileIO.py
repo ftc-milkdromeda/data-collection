b607aa1057217f70749b57c7b1ef17be48179f01
Tyler Wang <wangtyler123@gmail.com>
2021-01-02 15:38:49 -0600

    Used for saving and recalling files.

--- dependancies/FileIO.py
+++ dependancies/FileIO.py
@@ -1,14 +1,14 @@
 import pickle as pkl
 
 def save(name, data):
-    file = open(name + ".dt", "wb")
+    file = open(name + "-pk.dt", "wb")
     file.truncate(0)
 
     pkl.dump(data, file)
     file.close()
 
 def recall(name):
-    file = open(name + ".dt", "rb")
+    file = open(name + "-pk.dt", "rb")
     data = pkl.load(file)
     file.close()
 
